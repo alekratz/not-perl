caa4d61ae60969c08f7c97d9b5f52983a2f1a757
croy <craigr63@gmail.com>
2018-11-03 20:02:23 +0000

    Fix warnings and errors when running tests
    Tests were failing for two reasons:
    - PartialEq was not implemented for SrcRange
    - A Range field was added to FunStub, but the tests didn't include it
    
    The rest of the changes were to clean up warnings from the build

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,6 @@ fn exec(mut args: Args) -> Result<(), common::ProcessError> {
     args.next().expect("exec() must be called with at least 2 args");
     let path = args.next()
         .expect("exec() must be called with at least 2 args");
-    let args: Vec<_> = args.collect();
     let mut compile = compile::Compile::new();
     compile.update_from_path(&path)
 }
